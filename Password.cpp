56157692836e2f3f8b208ff1f559847dcf162eaf
TakingFire <bacurrah@gmail.com>
2025-11-05 11:42:26 -0800

    Fix failed test empty_string

--- Password.cpp
+++ Password.cpp
@@ -4,12 +4,13 @@
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
+  The function receives a string counts how many times the same character
   occurs at the beginning of the string, before any other characters (or the
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  if (phrase.empty()) return 0;
   int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
