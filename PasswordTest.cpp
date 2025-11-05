5c4f977570db7d7abab3a9854c0e96b4c5881707
TakingFire <bacurrah@gmail.com>
2025-11-05 11:25:04 -0800

    Add unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,58 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, empty_string)
 {
-	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	Password password;
+	int result = password.count_leading_characters("");
+	ASSERT_EQ(0, result);
+}
+
+TEST(PasswordTest, single_letter)
+{
+	Password password;
+	int result = password.count_leading_characters("a");
+	ASSERT_EQ(1, result);
+}
+
+TEST(PasswordTest, single_letter_stop)
+{
+	Password password;
+	int result = password.count_leading_characters("ab");
+	ASSERT_EQ(1, result);
+}
+
+TEST(PasswordTest, double_letter)
+{
+	Password password;
+	int result = password.count_leading_characters("aa");
+	ASSERT_EQ(2, result);
+}
+
+TEST(PasswordTest, double_letter_stop)
+{
+	Password password;
+	int result = password.count_leading_characters("aab");
+	ASSERT_EQ(2, result);
+}
+
+TEST(PasswordTest, mixed_case)
+{
+	Password password;
+	int result = password.count_leading_characters("aA");
+	ASSERT_EQ(1, result);
+}
+
+TEST(PasswordTest, non_contiguous)
+{
+	Password password;
+	int result = password.count_leading_characters("aba");
+	ASSERT_EQ(1, result);
+}
+
+TEST(PasswordTest, escape_character)
+{
+	Password password;
+	int result = password.count_leading_characters("\n\n");
+	ASSERT_EQ(2, result);
 }
